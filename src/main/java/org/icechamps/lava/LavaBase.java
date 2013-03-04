bd611e0768f0117f9c195effbf330a217707e247
Robbie Diaz <metaldrummer610@gmail.com>
2013-03-04 18:14:17 -0500

    Added a number of methods

--- src/main/java/org/icechamps/lava/LavaBase.java
+++ src/main/java/org/icechamps/lava/LavaBase.java
@@ -365,6 +365,124 @@ public class LavaBase {
     }
 
     ///////////////
+    // Max
+    ///////////////
+
+    /**
+     * Returns the largest value from the collection, using the default comparison method
+     *
+     * @param collection The collection to search
+     * @param <T>        The type of the collection
+     * @return The largest value in from the collection
+     */
+    protected <T extends Comparable<? super T>> T max(Collection<T> collection) {
+        Preconditions.checkNotNull(collection);
+
+        T ret = null;
+        for (T t : collection) {
+            if (ret == null) {
+                ret = t;
+                continue;
+            }
+
+            if (ret.compareTo(t) > 0) {
+                ret = t;
+            }
+        }
+
+        return ret;
+    }
+
+    /**
+     * Returns the largest value from the collection using the callback function to determine the value to be compared.
+     *
+     * @param collection The collection to search
+     * @param func       The callback function used to generate the value
+     * @param <T>        The type of the object in the collection
+     * @param <E>        The type of the object returned from the callback function
+     * @return The largest value from the collection
+     */
+    protected <T, E extends Comparable<? super E>> E max(Collection<T> collection, Func<T, E> func) {
+        Preconditions.checkNotNull(collection);
+        Preconditions.checkNotNull(func);
+
+        E ret = null;
+
+        for (T t : collection) {
+            E e = func.callback(t);
+            if (ret == null) {
+                ret = e;
+                continue;
+            }
+
+            if (ret.compareTo(e) > 0) {
+                ret = e;
+            }
+        }
+
+        return ret;
+    }
+
+    ///////////////
+    // Min
+    ///////////////
+
+    /**
+     * Returns the smallest value from the collection, using the default comparison method
+     *
+     * @param collection The collection to search
+     * @param <T>        The type of the collection
+     * @return The smallest value in from the collection
+     */
+    protected <T extends Comparable<? super T>> T min(Collection<T> collection) {
+        Preconditions.checkNotNull(collection);
+
+        T ret = null;
+        for (T t : collection) {
+            if (ret == null) {
+                ret = t;
+                continue;
+            }
+
+            if (ret.compareTo(t) < 0) {
+                ret = t;
+            }
+        }
+
+        return ret;
+    }
+
+    /**
+     * Returns the smallest value from the collection using the callback function to determine the value to be compared.
+     *
+     * @param collection The collection to search
+     * @param func       The callback function used to generate the value
+     * @param <T>        The type of the object in the collection
+     * @param <E>        The type of the object returned from the callback function
+     * @return The smallest value from the collection
+     */
+    protected <T, E extends Comparable<? super E>> E min(Collection<T> collection, Func<T, E> func) {
+        Preconditions.checkNotNull(collection);
+        Preconditions.checkNotNull(func);
+
+        E ret = null;
+
+        for (T t : collection) {
+            E e = func.callback(t);
+            if (ret == null) {
+                ret = e;
+                continue;
+            }
+
+            if (ret.compareTo(e) < 0) {
+                ret = e;
+            }
+        }
+
+        return ret;
+    }
+
+    ///////////////
     // Order By
     ///////////////
 
@@ -413,6 +531,37 @@ public class LavaBase {
     }
 
     ///////////////
+    // Order By Descending
+    ///////////////
+
+    /**
+     * Orders the given collection using a default comparator in reverse
+     *
+     * @param collection The collection to order
+     * @param <T>        The type of the object in the collection
+     * @return The sorted collection
+     */
+    protected <T extends Comparable<? super T>> LavaList<T> orderByDescending(Collection<T> collection) {
+        LavaList<T> ret = orderByListInternal(collection, null);
+        Collections.reverse(ret);
+        return ret;
+    }
+
+    /**
+     * Orders the given collection using the given comparator in reverse
+     *
+     * @param collection The collection to order
+     * @param comparator The comparator to use
+     * @param <T>        The type of the object in the collection
+     * @return The sorted collection
+     */
+    protected <T extends Comparable<? super T>> LavaList<T> orderByDescending(Collection<T> collection, Comparator<T> comparator) {
+        LavaList<T> ret = orderByListInternal(collection, comparator);
+        Collections.reverse(ret);
+        return ret;
+    }
+
+    ///////////////
     // Select
     ///////////////
 
@@ -437,6 +586,70 @@ public class LavaBase {
         }
 
         return ret;
+    }
+
+    ///////////////
+    // Select Many
+    ///////////////
+
+    /**
+     * Preforms a one to many projection from the source collection to a resulting collection
+     *
+     * @param sourceCollection The source collection
+     * @param collectionFunc   The function that returns a collection based on the element passed in
+     * @param resultFunc       The function that generates a new result based on the collection and source element
+     * @param <Source>         The type of object in the source collection
+     * @param <TCollection>    The type of object in the projected collection
+     * @param <Result>         The type of object that is to be returned from the projection
+     * @return A collection of the resulting objects from the result function
+     */
+    protected <Source, TCollection, Result> LavaCollection<Result> selectMany(Collection<Source> sourceCollection,
+                                                                              Func<Source, Collection<TCollection>> collectionFunc,
+                                                                              Func2<Source, TCollection, Result> resultFunc) {
+        Preconditions.checkNotNull(sourceCollection);
+        Preconditions.checkNotNull(collectionFunc);
+        Preconditions.checkNotNull(resultFunc);
+
+        LavaCollection<Result> results = buildLavaCollectionFromCollection(sourceCollection);
+        for (Source source : sourceCollection) {
+            for (TCollection collection : collectionFunc.callback(source)) {
+                results.add(resultFunc.callback(source, collection));
+            }
+        }
+
+        return results;
+    }
+
+    ///////////////
+    // Sequence Equal
+    ///////////////
+
+    /**
+     * Checks both collections to see if they are equal. "Equal" is defined as: having the same size, and having the objects in the same order.
+     * If the two collections contain the same objects, but those objects are in different orders, then they are not considered "equal" by this method.
+     *
+     * @param first  The first collection
+     * @param second The second collection
+     * @param <T>    The type of object in both collections
+     * @return True if they are equal, as defined above, and false if they are not.
+     */
+    protected <T> boolean sequenceEqual(Collection<T> first, Collection<T> second) {
+        Preconditions.checkNotNull(first);
+        Preconditions.checkNotNull(second);
+
+        if (first.size() != second.size())
+            return false;
+
+        int size = first.size();
+        Iterator<T> firstIter = first.iterator();
+        Iterator<T> secondIter = second.iterator();
+
+        while (firstIter.hasNext() && secondIter.hasNext()) {
+            if (!firstIter.next().equals(secondIter.next()))
+                return false;
+        }
+
+        return true;
     }
 
     ///////////////
@@ -508,6 +721,343 @@ public class LavaBase {
     }
 
     ///////////////
+    // Skip
+    ///////////////
+
+    /**
+     * Skips {@code count} number of elements in the given collection, then returns the rest of the collection
+     *
+     * @param collection The collection to use
+     * @param count      The number of elements of the collection to skip
+     * @param <T>        The type of the elements in the collection
+     * @return The elements in the collection minus the first {@code count} elements
+     */
+    protected <T> LavaCollection<T> skip(Collection<T> collection, int count) {
+        Preconditions.checkNotNull(collection);
+        Preconditions.checkArgument(count >= 0);
+        Preconditions.checkArgument(count > collection.size());
+
+        LavaCollection<T> ret = buildLavaCollectionFromCollection(collection);
+
+        if (count == 0) {
+            ret.addAll(collection);
+            return ret;
+        }
+
+        Iterator<T> iterator = collection.iterator();
+        for (int i = 0; i < count; i++) {
+            iterator.next();
+        }
+
+        while (iterator.hasNext()) {
+            ret.add(iterator.next());
+        }
+
+        return ret;
+    }
+
+    ///////////////
+    // Skip While
+    ///////////////
+
+    /**
+     * Skips elements in the collection as long as the callback function returns true
+     *
+     * @param collection The collection to use
+     * @param func       The callback function used to test each element
+     * @param <T>        The type of object in the collection
+     * @return A subset of the collection minus the elements the callback function filtered out
+     */
+    protected <T> LavaCollection<T> skipWhile(Collection<T> collection, Func<T, Boolean> func) {
+        Preconditions.checkNotNull(collection);
+        Preconditions.checkNotNull(func);
+
+        Iterator<T> iterator = collection.iterator();
+        while (func.callback(iterator.next())) ;
+
+        LavaCollection<T> ret = buildLavaCollectionFromCollection(collection);
+        while (iterator.hasNext()) {
+            ret.add(iterator.next());
+        }
+
+        return ret;
+    }
+
+    ///////////////
+    // Sum
+    ///////////////
+
+    /**
+     * Sums up the collection and returns the results
+     *
+     * @param collection The collection to sum
+     * @return The added values of the collection
+     */
+    protected Byte sum(Collection<Byte> collection) {
+        Preconditions.checkNotNull(collection);
+
+        Byte ret = new Byte((byte) 0);
+
+        for (Byte b : collection) {
+            ret += b;
+        }
+
+        return ret;
+    }
+
+    /**
+     * Sums up the collection and returns the results
+     *
+     * @param collection The collection to sum
+     * @return The added values of the collection
+     */
+    protected Double sum(Collection<Double> collection) {
+        Preconditions.checkNotNull(collection);
+
+        Double ret = new Double(0);
+
+        for (Double d : collection) {
+            ret += d;
+        }
+
+        return ret;
+    }
+
+    /**
+     * Sums up the collection and returns the results
+     *
+     * @param collection The collection to sum
+     * @return The added values of the collection
+     */
+    protected Float sum(Collection<Float> collection) {
+        Preconditions.checkNotNull(collection);
+
+        Float ret = new Float(0);
+
+        for (Float f : collection) {
+            ret += f;
+        }
+
+        return ret;
+    }
+
+    /**
+     * Sums up the collection and returns the results
+     *
+     * @param collection The collection to sum
+     * @return The added values of the collection
+     */
+    protected Integer sum(Collection<Integer> collection) {
+        Preconditions.checkNotNull(collection);
+
+        Integer ret = new Integer(0);
+
+        for (Integer i : collection) {
+            ret += i;
+        }
+
+        return ret;
+    }
+
+    /**
+     * Sums up the collection and returns the results
+     *
+     * @param collection The collection to sum
+     * @return The added values of the collection
+     */
+    protected Long sum(Collection<Long> collection) {
+        Preconditions.checkNotNull(collection);
+
+        Long ret = new Long(0);
+
+        for (Long l : collection) {
+            ret += l;
+        }
+
+        return ret;
+    }
+
+    /**
+     * Sums up the collection and returns the results
+     *
+     * @param collection The collection to sum
+     * @return The added values of the collection
+     */
+    protected Short sum(Collection<Short> collection) {
+        Preconditions.checkNotNull(collection);
+
+        Short ret = new Short((short) 0);
+
+        for (Short s : collection) {
+            ret += s;
+        }
+
+        return ret;
+    }
+
+    /**
+     * Sums up the collection and returns the results
+     *
+     * @param collection The collection to sum
+     * @param func       The callback function that does the addition. The first argument is the result of all the sums so far. The second is the current item in the iteration. The result is the addition of the two. On first run, the first argument will be null.
+     * @param <T>        The type of object in the collection
+     * @return The added values of the collection
+     */
+    protected <T> T sum(Collection<T> collection, Func2<T, T, T> func) {
+        Preconditions.checkNotNull(collection);
+        Preconditions.checkNotNull(func);
+
+        if (collection.isEmpty()) {
+            return null;
+        }
+
+        Iterator<T> iterator = collection.iterator();
+        T ret = func.callback(null, iterator.next());
+
+        while (iterator.hasNext()) {
+            ret = func.callback(ret, iterator.next());
+        }
+
+        return ret;
+    }
+
+    ///////////////
+    // Take
+    ///////////////
+
+    /**
+     * Takes the first {@code count} elements from the collection and returns them in a new collection.
+     *
+     * @param collection The collection to use
+     * @param count      The number of elements to take
+     * @param <T>        The type of the elements
+     * @return The first {@code count} elements in the collection
+     * @throws IndexOutOfBoundsException If count is larger than the number of elements in the collection
+     */
+    protected <T> LavaCollection<T> take(Collection<T> collection, int count) {
+        Preconditions.checkNotNull(collection);
+        Preconditions.checkArgument(count >= 0);
+
+        LavaCollection<T> ret = buildLavaCollectionFromCollection(collection);
+
+        if (count == 0 || collection.isEmpty())
+            return ret;
+
+        Iterator<T> iterator = collection.iterator();
+        for (int i = 0; i < count; i++) {
+            if (iterator.hasNext())
+                ret.add(iterator.next());
+            else
+                throw new IndexOutOfBoundsException();
+        }
+
+        return ret;
+    }
+
+    ///////////////
+    // Take While
+    ///////////////
+
+    /**
+     * Takes elements from the beginning of the collection, as long as the callback returns true, and puts them, in order, into a resulting collection.
+     *
+     * @param collection The collection to search
+     * @param func       The callback function to use to test the elements
+     * @param <T>        The type of the elements in the list
+     * @return The resulting elements from the collection
+     */
+    protected <T> LavaCollection<T> takeWhile(Collection<T> collection, Func<T, Boolean> func) {
+        Preconditions.checkNotNull(collection);
+        Preconditions.checkNotNull(func);
+
+        LavaCollection<T> ret = buildLavaCollectionFromCollection(collection);
+
+        if (collection.isEmpty())
+            return ret;
+
+        Iterator<T> iterator = collection.iterator();
+
+        while (iterator.hasNext()) {
+            T next = iterator.next();
+            if (func.callback(next))
+                ret.add(next);
+        }
+
+        return ret;
+    }
+
+    ///////////////
+    // To Map
+    ///////////////
+
+    /**
+     * Projects the given collection to a {@link Map} type using the callback function. The callback will generate a key based on the given value. The resulting key will be paired with the value and added to the map.
+     *
+     * @param collection The collection to project
+     * @param func       The function to generate keys for the values
+     * @param <Key>      The type of the key
+     * @param <Value>    The type of the value
+     * @return The new Map instance containing the keys and values from the collection
+     */
+    protected <Key, Value> Map<Key, Value> toMap(Collection<Value> collection, Func<Value, Key> func) {
+        Preconditions.checkNotNull(collection);
+        Preconditions.checkNotNull(func);
+
+        if (collection.isEmpty()) {
+            return new HashMap<Key, Value>();
+        }
+
+        HashMap<Key, Value> ret = new HashMap<Key, Value>(collection.size());
+
+        for (Value value : collection) {
+            Key key = func.callback(value);
+            ret.put(key, value);
+        }
+
+        return ret;
+    }
+
+    ///////////////
+    // To List
+    ///////////////
+
+    /**
+     * Creates a new {@link LavaList} from the given collection
+     *
+     * @param collection The collection to use
+     * @param <T>        The type of object in the collection
+     * @return The new LavaList containing the elements in the collection
+     */
+    protected <T> LavaList<T> toList(Collection<T> collection) {
+        Preconditions.checkNotNull(collection);
+
+        LavaList<T> ret = new LavaList<T>();
+        ret.addAll(collection);
+
+        return ret;
+    }
+
+    ///////////////
+    // To Set
+    ///////////////
+
+    /**
+     * Creates a new {@link LavaSet} from the given collection
+     *
+     * @param collection The collection to use
+     * @param <T>        The type of object in the collection
+     * @return A new LavaSet containing the objects in the collection
+     */
+    protected <T> LavaSet<T> toSet(Collection<T> collection) {
+        Preconditions.checkNotNull(collection);
+
+        LavaSet<T> set = new LavaSet<T>();
+        set.addAll(collection);
+
+        return set;
+    }
+
+    ///////////////
     // Where
     ///////////////
 
@@ -553,6 +1103,7 @@ public class LavaBase {
         return ret;
     }
 
-    //TODO: Add the following methods: Join, Max, Min, OrderByDescending, SelectMany, SequenceEqual, Skip, SkipWhile, Sum, Take, TakeWhile, ThenBy, ToMap, ToList, ToSet, Union, Zip
+    //TODO: Add the following methods: Union, Zip
+    //TODO: Phase 2: Average, Cast, Concat, ElementAt?, ElementAtOrDefault?, Except, GroupBy, GroupJoin, Join, OfType, Range, Repeat, Reverse
 
 }
